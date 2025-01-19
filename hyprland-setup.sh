e9dbf8d2905b44a288beb2e8f4843bb8c16d98f2
JakubCzarnota <kuba.czarnota05@outlook.com>
2025-01-19 13:44:51 +0100

    portals

--- hyprland-setup.sh
+++ hyprland-setup.sh
@@ -2,4 +2,4 @@
 
 yay -Sy hyprland hypridle hyprlock hyprpaper hyprshot swaync starship ttf-cascadia-code-nerd catppuccin-gtk-theme-mocha nwg-look
 
-sudo pacman -S wofi waybar ttf-font-awesome
+sudo pacman -S wofi waybar ttf-font-awesome xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
