1e32469a70379e1658ef48e3a4bfed85dc9dc12f
JakubCzarnota <kuba.czarnota05@outlook.com>
2025-01-22 23:43:48 +0100

    2025-01-22

--- hyprland-setup.sh
+++ hyprland-setup.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
 
-yay -Sy hyprland hypridle hyprlock hyprpaper hyprshot swaync starship ttf-cascadia-code-nerd catppuccin-gtk-theme-mocha nwg-look
+yay -Sy hyprland hypridle hyprlock hyprpaper hyprshot swaync starship hyprpolkitagent ttf-cascadia-code-nerd catppuccin-gtk-theme-mocha nwg-look
 
 sudo pacman -S wofi waybar ttf-font-awesome xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
