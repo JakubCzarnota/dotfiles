1e4e7e628e409861349d32725b9a11a7b03cc49e
JakubCzarnota <kuba.czarnota05@outlook.com>
2025-09-14 14:40:52 +0200

    14/09/2025

--- hyprland-setup.sh
+++ hyprland-setup.sh
@@ -2,4 +2,4 @@
 
 yay -Sy hyprland hypridle hyprlock hyprpaper hyprshot swaync starship hyprpolkitagent ttf-cascadia-code-nerd catppuccin-gtk-theme-mocha nwg-look
 
-sudo pacman -S wofi waybar ttf-font-awesome xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
+sudo pacman -S wofi waybar ttf-font-awesome xdg-desktop-portal-hyprland xdg-desktop-portal-gtk paf-notify
